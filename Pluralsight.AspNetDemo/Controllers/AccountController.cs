b2de49ec35807cf31cb011f9502d4ff0cb0b5aaf
Marcos Robles <ecko_lob@outlook.com>
2020-07-23 17:56:41 -0500

    Agregar archivos de proyecto.

---
+++ Pluralsight.AspNetDemo/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Pluralsight.AspNetDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pluralsight.AspNetDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        public UserManager<IdentityUser> UserManager => HttpContext.GetOwinContext().Get<UserManager<IdentityUser>>();
+
+
+
+        public ActionResult Register() {
+            return View();
+        }
+
+
+        [HttpPost]
+        public  async Task<ActionResult> Register(RegisterModel model) {
+
+
+        var identityResult =  await  UserManager.CreateAsync(new IdentityUser(model.UserName), model.Password);
+            if (identityResult.Succeeded) {
+                return RedirectToAction("Index","Home");
+            }
+
+            ModelState.AddModelError("", identityResult.Errors.FirstOrDefault());
+            return View(model);
+        }
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Pluralsight.AspNetDemo/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Pluralsight.AspNetDemo.Startup))]
+
+namespace Pluralsight.AspNetDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Para obtener más información sobre cómo configurar la aplicación, visite https://go.microsoft.com/fwlink/?LinkID=316888
+
+            const string connectionstring = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=Pluralsight.AspNetIdentityDemo.Module2.2;Integrated Security=SSPI;";
+            app.CreatePerOwinContext(() => new IdentityDbContext(connectionstring));
+            app.CreatePerOwinContext<UserStore<IdentityUser>>((opt, cont) => new UserStore<IdentityUser>(cont.Get<IdentityDbContext>()));
+            app.CreatePerOwinContext<UserManager<IdentityUser>>(
+                (opt, cont) => new UserManager<IdentityUser>(cont.Get<UserStore<IdentityUser>>()));
+        }
+    }
+}
